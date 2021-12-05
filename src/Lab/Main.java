f0e07caf64beaf15acfcfbaeac644dd529a3699e
KaderKirill <K1436114k>
2021-12-05 18:40:54 +0300

    .

---
+++ src/Lab/Main.java
@@ -0,0 +1,28 @@
+package Lab;
+
+import Lab.classesAndObjects.ballAndBasket.Ball;
+import Lab.classesAndObjects.ballAndBasket.Basket;
+import Lab.classesAndObjects.ballAndBasket.Color;
+import Lab.javaFundamental.Task5;
+import Lab.javaFundamental.task2.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Task2 task2 = new Task2();
+        System.out.println(task2.isInside(0, 0));
+        Task5 task5 = new Task5();
+        System.out.println(task5.findMinNumber(new double[]{3, 5, 25, 13, 0, 5, 23, 38, 9, 1}));
+
+        Ball ball1 = new Ball(12, Color.BLACK);
+        Ball ball2 = new Ball(15, Color.BLUE);
+        Ball ball3 = new Ball(6, Color.GREEN);
+        Ball ball4 = new Ball(2.5, Color.RED);
+        Ball ball5 = new Ball(1, Color.BLUE);
+        Ball ball6 = new Ball(3.0, Color.BLUE);
+        Ball ball7 = new Ball(14, Color.GREEN);
+
+        Basket basket = new Basket(ball1, ball2, ball3, ball4, ball5, ball6, ball7);
+        System.out.println(basket.calculateWeightBlue());
+        System.out.println(basket.calculateAmountBlue());
+    }
+}
---
+++ src/Lab/javaFundamental/Task4.java
@@ -0,0 +1,32 @@
+package Lab.javaFundamental;
+
+public class Task4 {
+
+    public void findPrimaryNumbers(double[] allNumbers) {
+        findPrimaryNumbers(allNumbers, allNumbers.length);
+    }
+
+    public void findPrimaryNumbers(double[] allNumbers, double N) {
+        if (N > allNumbers.length) {
+            N = allNumbers.length;
+        }
+        for (int i = 0; i < N; i++) {
+            if (isPrimary(allNumbers[i])) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private boolean isPrimary(double num) {
+        double divider = 2;
+        double rootNum = Math.sqrt(num)+1;
+        while (divider <= rootNum) {
+            if (num % divider == 0) {
+                return false;
+            }
+            divider++;
+        }
+        return true;
+    }
+}
+
